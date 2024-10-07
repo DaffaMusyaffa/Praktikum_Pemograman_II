ddb93a7d1db55627f0f68fea30bccc98d19be8b0
unknown <daffamusyaffa10@gmail.com>
2024-10-07 10:49:10 +0700

    Membenarkan

--- PERTEMUAN_2/Latihan_7_Mod/LATIHAN_7_MOD.java
+++ PERTEMUAN_2/Latihan_7_Mod/LATIHAN_7_MOD.java
@@ -1,242 +1,208 @@
 package PERTEMUAN_2.Latihan_7_Mod;
 
 import javax.swing.*;
-import javax.swing.event.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
-public class LATIHAN_7_MOD extends JFrame implements ActionListener, ItemListener, ChangeListener {
-
-    private JTextField kotakNama;
-    private JTextField kotakNoHp;
-    private JButton tombolSimpan;
-    private JButton tombolReset;
-    private JTextArea areaBiodata;
-    private JCheckBox checkBox;
-    private JRadioButton radio1;
-    private JRadioButton radio2;
-    private ButtonGroup bg;
-    private boolean isCheckBoxSelected;
-    private JList<String> listJenisTabungan;
-    private DefaultListModel<String> listModel;
-    private JMenuBar menuBar;
-    private JSlider slider;
-    private JSpinner spinner;
+public class LATIHAN_7_MOD extends JFrame {
+    private JTextField textFieldNama, textFieldHP;
+    private JRadioButton radioLakiLaki, radioPerempuan;
+    private JCheckBox checkBoxWNA;
+    private JTextArea txtOutput;
+    private JList<String> listTabungan;
+    private JSpinner spinnerTransaksi, spinnerTanggalLahir; // JSpinner untuk frekuensi transaksi dan tanggal lahir
+    private JPasswordField passwordField, confirmPasswordField; // JPasswordField untuk password
+    private JScrollPane scrollPane; // JScrollPane untuk membungkus JTextArea
 
     public LATIHAN_7_MOD() {
-        this.setTitle("FORM PENDAFTARAN NASABAH BANK");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 750);
+        this.setSize(500, 850); // Update ukuran frame
+        this.setTitle("Aplikasi BiodataMod");
         this.setLayout(null);
 
-        // Menu
-        menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        // Setup Menu
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+        JMenuItem menuItemReset = new JMenuItem("Reset");
+        JMenuItem menuItemExit = new JMenuItem("Exit");
 
-        // Field Nama
-        JLabel LabelNama = new JLabel("Nama :");
-        LabelNama.setBounds(15, 20, 100, 30);
-        kotakNama = new JTextField();
-        kotakNama.setBounds(85, 20, 200, 30);
+        menuItemReset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetFields();
+            }
+        });
 
-        // Field No HP
-        JLabel LabelNoHp = new JLabel("Nomor Hp :");
-        LabelNoHp.setBounds(15, 60, 100, 30);
-        kotakNoHp = new JTextField();
-        kotakNoHp.setBounds(85, 60, 200, 30);
+        menuItemExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0); // Exit the application
+            }
+        });
 
-        // JSpinner Tanggal Lahir
-        JLabel spinnerLabel = new JLabel("Tanggal Lahir : ");
-        spinnerLabel.setBounds(15, 100, 350, 30);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, 1990);
-        calendar.set(Calendar.MONTH, 0);
-        calendar.set(Calendar.DATE, 1);
-        SpinnerDateModel model = new SpinnerDateModel(calendar.getTime(), null, null, calendar.DAY_OF_MONTH);
-        spinner = new JSpinner(model);
-        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(spinner, "dd-MM-yyyy");
-        spinner.setEditor(dateEditor);
-        spinner.setBounds(15, 130, 350, 30);
-
-        // Radio Jenis Kelamin
-        JLabel jenisKelamin = new JLabel("Jenis Kelamin : ");
-        jenisKelamin.setBounds(15, 170, 100, 30);
-        radio1 = new JRadioButton("Laki-Laki");
-        radio1.setBounds(15, 195, 350, 30);
-        radio2 = new JRadioButton("Perempuan");
-        radio2.setBounds(15, 225, 350, 30);
-        bg = new ButtonGroup();
-        bg.add(radio1);
-        bg.add(radio2);
-
-        // Checkbox
-        checkBox = new JCheckBox("Warga Negara Asing");
-        checkBox.setBounds(15, 250, 350, 30);
-        checkBox.addItemListener(this);
-
-        // JList Tabungan
-        JLabel jenisTabungan = new JLabel("Jenis Tabungan : ");
-        jenisTabungan.setBounds(15, 280, 100, 30);
-        listModel = new DefaultListModel<>();
-        listModel.addElement("Tabungan Rumah");
-        listModel.addElement("Tabungan Investasi");
-        listModel.addElement("Tabungan Pendidikan");
-        listModel.addElement("Tabungan Haji");
-        listJenisTabungan = new JList<>(listModel);
-        listJenisTabungan.setBounds(15, 306, 200, 75);
-        listJenisTabungan.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-        // Button Simpan
-        tombolSimpan = new JButton("Simpan");
-        tombolSimpan.setBounds(15, 625, 100, 30);
-        tombolSimpan.addActionListener(this);
-
-        // Button Reset
-        tombolReset = new JButton("Reset");
-        tombolReset.setBounds(150, 625, 100, 30);
-        tombolReset.addActionListener(this);
-
-        // JTextArea field menampung nilai
-        areaBiodata = new JTextArea("");
-        areaBiodata.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(areaBiodata);
-        scrollPane.setBounds(15, 515, 280, 100);
-
-        // JSlider
-        JLabel sliderLabel = new JLabel("Frekuensi transaksi per bulan : ");
-        sliderLabel.setBounds(15, 385, 500, 30);
-        slider = new JSlider(0, 100, 0);
-        slider.setBounds(15, 425, 380, 75);
-        slider.setPaintTicks(true);
-        slider.setMinorTickSpacing(5);
-        slider.setMajorTickSpacing(10);
-        slider.setPaintLabels(true);
-
-
-        
-
-        menuBar.add(fileMenu);
-        fileMenu.add(exitMenuItem);
+        menu.add(menuItemReset);
+        menu.add(menuItemExit);
+        menuBar.add(menu);
         this.setJMenuBar(menuBar);
 
-        exitMenuItem.addActionListener(e -> System.exit(0));
+        // Labels and Fields
+        JLabel labelNama = new JLabel("Nama:");
+        labelNama.setBounds(15, 20, 350, 20);
+        this.add(labelNama);
 
-        add(LabelNama);
-        add(kotakNama);
-        add(LabelNoHp);
-        add(kotakNoHp);
-        add(spinnerLabel);
-        add(spinner);
-        add(jenisKelamin);
-        add(radio1);
-        add(radio2);
-        add(checkBox);
-        add(jenisTabungan);
-        add(listJenisTabungan);
-        add(sliderLabel);
-        add(slider);
-        add(tombolSimpan);
-        add(tombolReset);
-        add(scrollPane);
+        textFieldNama = new JTextField();
+        textFieldNama.setBounds(15, 50, 350, 30);
+        this.add(textFieldNama);
 
-        setVisible(true);
-    }
+        JLabel labelHP = new JLabel("Nomor HP:");
+        labelHP.setBounds(15, 90, 350, 20);
+        this.add(labelHP);
 
-    @Override
-    public void stateChanged(ChangeEvent e) {
-        
-    }
+        textFieldHP = new JTextField();
+        textFieldHP.setBounds(15, 120, 350, 30);
+        this.add(textFieldHP);
 
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-        isCheckBoxSelected = e.getStateChange() == ItemEvent.SELECTED;
-    }
+        JLabel labelJenisKelamin = new JLabel("Jenis Kelamin:");
+        labelJenisKelamin.setBounds(15, 150, 100, 20);
+        this.add(labelJenisKelamin);
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == tombolSimpan) {
-            JPasswordField passwordField = new JPasswordField();
-            int masukkanPassword = JOptionPane.showConfirmDialog(
-                    this,
-                    passwordField,
-                    "Masukkan Password Anda,",
-                    JOptionPane.OK_CANCEL_OPTION,
-                    JOptionPane.PLAIN_MESSAGE);
+        radioLakiLaki = new JRadioButton("Laki-Laki");
+        radioLakiLaki.setBounds(15, 180, 350, 30);
+        this.add(radioLakiLaki);
 
-            if (masukkanPassword == JOptionPane.OK_OPTION) {
-                char[] password = passwordField.getPassword();
-                String passwordString = new String(password);
+        radioPerempuan = new JRadioButton("Perempuan");
+        radioPerempuan.setBounds(15, 210, 350, 30);
+        this.add(radioPerempuan);
 
-                if (passwordString.equals("P@ssw0rd")) {
-                    String nama = kotakNama.getText();
-                    String noHp = kotakNoHp.getText();
-                    Date tanggalLahir = (Date) spinner.getValue();
-                    String tanggal = new SimpleDateFormat("dd-MM-yyyy").format(tanggalLahir);
-                    String JK = "";
+        ButtonGroup group = new ButtonGroup();
+        group.add(radioLakiLaki);
+        group.add(radioPerempuan);
 
-                    if (radio1.isSelected()) {
-                        JK = radio1.getText();
-                    }
-                    if (radio2.isSelected()) {
-                        JK = radio2.getText();
-                    }
+        checkBoxWNA = new JCheckBox("Warga Negara Asing");
+        checkBoxWNA.setBounds(15, 240, 350, 30);
+        this.add(checkBoxWNA);
 
-                    String dataTabungan = listJenisTabungan.getSelectedValue() != null ? listJenisTabungan.getSelectedValue() : "Tidak ada tabungan yang dipilih";
+        JLabel labelTabungan = new JLabel("Jenis Tabungan:");
+        labelTabungan.setBounds(15, 270, 350, 20);
+        this.add(labelTabungan);
 
-                    String wnaStatus = isCheckBoxSelected ? "WNA : Ya" : "WNA : Bukan";
-                    int frekuensiTransaksi = slider.getValue();
+        String[] jenisTabungan = {"Tabungan Reguler", "Tabungan Pendidikan", "Tabungan Bisnis", "Tabungan Haji"};
+        listTabungan = new JList<>(jenisTabungan);
+        listTabungan.setBounds(15, 300, 350, 75);
+        listTabungan.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        this.add(listTabungan);
 
-                    String biodata = "Nama: " + nama +
-                            "\nNomor Telepon: " + noHp +
-                            "\nTanggal Lahir : " + tanggal +
-                            "\nJenis Kelamin : " + JK +
-                            "\n" + wnaStatus +
-                            "\nTabungan yang dipilih : " + dataTabungan +
-                            "\nFrekuensi Transaksi : " + frekuensiTransaksi + "$" +
-                            "\n" + "=".repeat(30) + "\n";
+        // Spinner untuk frekuensi transaksi
+        JLabel labelTransaksi = new JLabel("Frekuensi Transaksi per Bulan:");
+        labelTransaksi.setBounds(15, 390, 350, 20);
+        this.add(labelTransaksi);
 
-                    areaBiodata.append(biodata);
-                    JOptionPane.showMessageDialog(null, "Berhasil Menambah Data", "Title", JOptionPane.INFORMATION_MESSAGE);
-                }
+        SpinnerModel modelSpinner = new SpinnerNumberModel(1, 1, 100, 1);
+        spinnerTransaksi = new JSpinner(modelSpinner);
+        spinnerTransaksi.setBounds(280, 390, 50, 30);
+        this.add(spinnerTransaksi);
+
+        // Spinner untuk tanggal lahir
+        JLabel labelTanggalLahir = new JLabel("Tanggal Lahir:");
+        labelTanggalLahir.setBounds(15, 430, 350, 20);
+        this.add(labelTanggalLahir);
+
+        spinnerTanggalLahir = new JSpinner(new SpinnerDateModel());
+        spinnerTanggalLahir.setBounds(15, 460, 200, 30);
+        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(spinnerTanggalLahir, "dd/MM/yyyy");
+        spinnerTanggalLahir.setEditor(dateEditor);
+        this.add(spinnerTanggalLahir);
+
+        // Password Fields
+        JLabel labelPassword = new JLabel("Password:");
+        labelPassword.setBounds(15, 500, 350, 20);
+        this.add(labelPassword);
+
+        passwordField = new JPasswordField();
+        passwordField.setBounds(15, 530, 350, 30);
+        this.add(passwordField);
+
+        JLabel labelConfirmPassword = new JLabel("Confirm Password:");
+        labelConfirmPassword.setBounds(15, 570, 350, 20);
+        this.add(labelConfirmPassword);
+
+        confirmPasswordField = new JPasswordField();
+        confirmPasswordField.setBounds(15, 600, 350, 30);
+        this.add(confirmPasswordField);
+
+        JButton buttonSimpan = new JButton("Simpan");
+        buttonSimpan.setBounds(15, 640, 100, 30);
+        this.add(buttonSimpan);
+
+        // JTextArea for output
+        txtOutput = new JTextArea();
+        txtOutput.setEditable(false); // Disable user editing
+
+        // JScrollPane to wrap JTextArea
+        scrollPane = new JScrollPane(txtOutput);
+        scrollPane.setBounds(15, 680, 350, 150); // Set bounds for JScrollPane
+        this.add(scrollPane); // Add JScrollPane to frame
+
+        buttonSimpan.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                simpanBiodata();
             }
-        }
+        });
 
-        if (e.getSource() == tombolReset) {
-            int konfirmasi = JOptionPane.showConfirmDialog(this, "Apakah Anda yakin?", "Konfirmasi", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-            JPasswordField passwordField = new JPasswordField();
-            int masukkanPassword = JOptionPane.showConfirmDialog(
-                    this,
-                    passwordField,
-                    "Masukkan Password Anda,",
-                    JOptionPane.OK_CANCEL_OPTION,
-                    JOptionPane.PLAIN_MESSAGE);
+        this.setVisible(true);
+    }
 
-            if (masukkanPassword == JOptionPane.OK_OPTION) {
-                char[] password = passwordField.getPassword();
-                String passwordString = new String(password);
+    private void simpanBiodata() {
+        String nama = textFieldNama.getText();
+        String nomorHP = textFieldHP.getText();
+        String jenisKelamin = radioLakiLaki.isSelected() ? "Laki-Laki" : radioPerempuan.isSelected() ? "Perempuan" : "Tidak Dipilih";
+        String wna = checkBoxWNA.isSelected() ? "Ya" : "Bukan";
+        String tabungan = listTabungan.getSelectedValue() != null ? listTabungan.getSelectedValue() : "Tidak Dipilih";
+        int frekuensiTransaksi = (int) spinnerTransaksi.getValue();
 
-                if (passwordString.equals("P@ssw0rd")) {
-                    if (konfirmasi == JOptionPane.YES_OPTION) {
-                        kotakNama.setText("");
-                        kotakNoHp.setText("");
-                        radio1.setSelected(false);
-                        radio2.setSelected(false);
-                        checkBox.setSelected(false);
-                        listJenisTabungan.clearSelection();
-                        areaBiodata.setText("");
-                    }
-                    JOptionPane.showMessageDialog(null, "Berhasil Mereset Field", "Title", JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-        }
+        // Mendapatkan password dan confirm password
+        String password = new String(passwordField.getPassword());
+        String confirmPassword = new String(confirmPasswordField.getPassword());
+
+        // Memeriksa apakah password dan confirm password cocok
+        String passwordStatus = password.equals(confirmPassword) ? "Password cocok" : "Password tidak cocok";
+
+        // Mendapatkan tanggal lahir
+        Date tanggalLahir = (Date) spinnerTanggalLahir.getValue();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String tanggalLahirStr = sdf.format(tanggalLahir);
+
+        // Menampilkan output di JTextArea
+        txtOutput.append("Nama       : " + nama + "\n");
+        txtOutput.append("Nomor HP   : " + nomorHP + "\n");
+        txtOutput.append("Jenis Kelamin : " + jenisKelamin + "\n");
+        txtOutput.append("WNA        : " + wna + "\n");
+        txtOutput.append("Jenis Tabungan : " + tabungan + "\n");
+        txtOutput.append("Frekuensi Transaksi : " + frekuensiTransaksi + "\n");
+        txtOutput.append("Tanggal Lahir : " + tanggalLahirStr + "\n");
+        txtOutput.append("Status Password: " + passwordStatus + "\n");
+        txtOutput.append("============================================\n");
+
+        // Mengosongkan fields input
+        resetFields();
+    }
+
+    private void resetFields() {
+        textFieldNama.setText("");
+        textFieldHP.setText("");
+        radioLakiLaki.setSelected(false);
+        radioPerempuan.setSelected(false);
+        checkBoxWNA.setSelected(false);
+        listTabungan.clearSelection();
+        spinnerTransaksi.setValue(1); // Reset spinner ke nilai default 1
+        spinnerTanggalLahir.setValue(new Date()); // Reset spinner tanggal lahir ke tanggal sekarang
+        passwordField.setText(""); // Reset password
+        confirmPasswordField.setText(""); // Reset confirm password
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new LATIHAN_7_MOD();
-        });
+        SwingUtilities.invokeLater(() -> new LATIHAN_7_MOD());
     }
 }
